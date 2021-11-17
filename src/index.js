b234da9880a4c4f6690624bfa5285b7bd0836e10
Andr <62073230+bradttles@users.noreply.github.com>
2021-11-17 14:43:03 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  query,
+  setDoc,
+  updateDoc,
+  doc,
+  serverTimestamp,
+} from 'firebase/firestore';
+import { getFirebaseConfig } from './firebase-config.js';
+const firebaseAppConfig = getFirebaseConfig();
+initializeApp(firebaseAppConfig);
