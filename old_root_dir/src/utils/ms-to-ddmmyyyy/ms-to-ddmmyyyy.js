164a87ed3ad689482a3bc12fc830ba5c15c16d09
Andrew <andrewbradt@Davids-iMac.local>
2021-11-26 12:51:53 -0500

    Added code from old_root_dir/src/index.js to /src/App.js

--- old_root_dir/src/utils/ms-to-ddmmyyyy/ms-to-ddmmyyyy.js
+++
@@ -1,7 +0,0 @@
-export function msToDDMMYYYY(ms){
-    const dateObj = new Date(ms);
-    const year = dateObj.getFullYear();
-    const month = dateObj.getMonth()+1;
-    const date = dateObj.getDate();
-    return `${date}-${month}-${year}`;
-};
